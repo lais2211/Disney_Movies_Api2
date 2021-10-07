dbd82bf1a84e78f147bfec741d20cf42546e41cf
Laís C. de Sousa <lais.sousa1608@gmail.com>
2021-10-07 20:54:17 -0300

    Initial commit

---
+++ src/main/java/com/portfolio/disney_movies_api2/DisneyMoviesApi2Application.java
@@ -0,0 +1,13 @@
+package com.portfolio.disney_movies_api2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisneyMoviesApi2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DisneyMoviesApi2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/portfolio/disney_movies_api2/DisneyMoviesApi2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.disney_movies_api2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisneyMoviesApi2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
